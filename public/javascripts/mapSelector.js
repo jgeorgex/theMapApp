ae987709f084780bd5dc7b189662140a13fb7de7
Julian George <jules104@gmail.com>
2020-01-26 14:31:50 +0000

    Add countries list

--- public/javascripts/mapSelector.js
+++ public/javascripts/mapSelector.js
@@ -1,3 +1,9 @@
+const { getCode, getName } = require("country-list");
+
+console.log(getName("IS")); // Iceland
+console.log(getCode("Iceland")); // IS
+console.log(getCode("Nowhere-to-be-found-land")); // undefined
+
 function selectCountry(country) {
   return country;
 }
